ffe9cec26e5aef16891fecb46b4fb01877d7925d
xavierrcz <96095850+xavierrcz@users.noreply.github.com>
2023-11-24 00:38:17 -0300

    Add files via upload

---
+++ main/java/com/example/trabalho2bim/activity/vews/view/PedidoActivity.java
@@ -0,0 +1,16 @@
+package com.example.trabalho2bim.activity.vews.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.trabalho2bim.R;
+
+public class PedidoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pedido);
+    }
+}
